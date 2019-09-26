1535c562db3ffaf0b42f9d4772509e2c96fa552a
Valerio Magnago <valerio.magnago@hotmail.it>
2019-09-26 21:30:53 +0200

    fix: enviroment.sh remove rpath from pipeline executable

--- environment.sh
+++ environment.sh
@@ -13,11 +13,13 @@ then
       source "$(dirname "${BASH_SOURCE[0]}" )/aliases"
       printf "\t\t* \e[32m DONE!\n\e[0m"
   fi
-  LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$AR_ROOT/build
+  # Convert rpath to runpath
+  chrpath -c $AR_CATKIN_ROOT/devel/lib/libimage_elab_LIBRARY.so
+  chrpath -c $AR_CATKIN_ROOT/devel/lib/libimage_elab_nodelet.so
+  chrpath -c $AR_CATKIN_ROOT/devel/lib/image_elab/*
+
+  # Add student build folder shared library
+  LD_LIBRARY_PATH=$AR_ROOT/build:$LD_LIBRARY_PATH
 else
   printf "\t*\e[33m Seems that you already source this environment.\e[0m\n"  
 fi
-
-
-
-
